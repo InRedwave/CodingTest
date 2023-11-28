c33352d0873e4c8ee51d4960849e29a496ae4753
minjae <jisung1202@Naver.com>
2023-11-28 20:24:45 +0900

    2023.11.28 백준 추가

---
+++ src/backJoon/step/easy/Main_2743.java
@@ -0,0 +1,14 @@
+package backJoon.step.easy;
+
+import java.util.Scanner;
+
+public class Main_2743 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        System.out.println(input.length());
+
+    };
+};
